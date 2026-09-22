e7ba5cb7eba6fd7b48a7afdface2378a6a51ff2d
Aki Hirvilammi <aki.hirvilammi@gmail.com>
2026-09-22 21:52:47 +0300

    Add php-light/config.example.php

---
+++ php-light/config.example.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'dsn' => 'mysql:host=127.0.0.1;dbname=aigm;charset=utf8mb4',
+    'user' => 'iam',
+    'password' => 'CHANGE_ME',
+    'session_ttl_seconds' => 2592000,
+    'cookie_name' => 'iam_light',
+    'cookie_path' => '/iam',
+    'cookie_secure' => true,
+];
